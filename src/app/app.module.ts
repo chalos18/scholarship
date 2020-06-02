14e739144bfb04967d4a03d5374067c07152bf83
Ana Laura Oliveira Souza <ms.guinevere@hotmail.com>
2020-06-02 14:24:46 +1200

    Version one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './header/home/home.component';
+import { RatingComponent } from './header/rating/rating.component';
+import { BrandsComponent } from './header/brands/brands.component';
+import { ExplanationsComponent } from './header/explanations/explanations.component';
+import { SearchBoxComponent } from './header/search-box/search-box.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'brands',
+    component: BrandsComponent
+  },
+  {
+    path: 'rating',
+    component: RatingComponent
+  },
+  {
+    path: 'explanations',
+    component: ExplanationsComponent
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BrandsComponent,
+    RatingComponent,
+    ExplanationsComponent,
+    SearchBoxComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
