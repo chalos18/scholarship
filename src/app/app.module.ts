be88b3936b14086c787a7b2a1288a4c5d1c40ec0
Ana Laura Oliveira Souza <ms.guinevere@hotmail.com>
2020-06-08 14:29:13 +1200

    Best Version so far
    This is the version where the Header folder was deleted and everything is working fine. Next I will try to make  a server and make the input work well.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
 
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './header/home/home.component';
-import { RatingComponent } from './header/rating/rating.component';
-import { BrandsComponent } from './header/brands/brands.component';
-import { ExplanationsComponent } from './header/explanations/explanations.component';
-import { SearchBoxComponent } from './header/search-box/search-box.component';
-import { BrandMenuComponent } from './header/brands/brand-menu.component';
+import { HomeComponent } from './home/home.component';
+import { RatingComponent } from './rating/rating.component';
+import { BrandsComponent } from './brands/brands.component';
+import { ExplanationsComponent } from './explanations/explanations.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { BrandMenuComponent } from './brands/brand-menu.component';
 
 // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
 // and returns simulated server responses.
 // Remove it when a real server is ready to receive requests.
-HttpClientInMemoryWebApiModule.forRoot(
-  InMemoryDataService, { dataEncapsulation: false }
-)
 
 const routes: Routes = [
   {
