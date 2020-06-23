506f8ddb6d42803b2795d7b90b7e0d70bef9532a
Ana Laura Oliveira Souza <ms.guinevere@hotmail.com>
2020-06-24 11:02:29 +1200

    almost done

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -15,10 +15,27 @@ import { CompanyService } from '../company.service';
   styleUrls: ['./search-box.component.css']
 })
 export class SearchBoxComponent implements OnInit {
+  companies$: Observable<Company[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private companyService: CompanyService) { }
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.companies$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.companyService.searchCompanies(term)),
+    );
   }
 
 }
